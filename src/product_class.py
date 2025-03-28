7097eb96eff81bb8f7804cb81b502c0f2afb5f07
Mihail Sokolov <Kilavyk@yandex.ru>
2025-03-28 22:46:08 +0300

    added @classmethod @property @price.setter

--- src/product_class.py
+++ src/product_class.py
@@ -12,5 +12,25 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, new_product: dict):
+        return cls(new_product["name"], new_product["description"], new_product["price"], new_product["quantity"])
 
+    @property
+    def price(self):
+        """Геттер для цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """Сеттер для цены с проверкой"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            if new_price < self.__price:  # если цена понижается
+                confirmation = input(f"Вы уверены, что хотите понизить цену с {self.__price} до {new_price}? (y/n): ")
+                if confirmation.lower() == 'y':
+                    self.__price = new_price
+            else:
+                self.__price = new_price
 
