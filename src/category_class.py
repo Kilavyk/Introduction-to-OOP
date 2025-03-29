7319fbae23d1e55536a11e8a17ad49cb1e1a906a
Mihail Sokolov <Kilavyk@yandex.ru>
2025-03-29 15:16:32 +0300

    after linters

--- src/category_class.py
+++ src/category_class.py
@@ -7,19 +7,20 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products=None):
+    def __init__(self, name, description, products=None) -> None:
+        """Инициализация категории."""
         self.name = name
         self.description = description
         self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
-    def add_product(self, product):
+    def add_product(self, product) -> None:
         """Добавляет товар в категорию"""
         self.__products.append(product)
         Category.product_count += 1
 
     @property
-    def products(self):
-        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
-                for product in self.__products]
+    def products(self) -> list:
+        """Геттер для получения форматированного списка товаров."""
+        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products]
--- src/product_class.py
+++ src/product_class.py
@@ -7,6 +7,7 @@ class Product:
     quantity: int
 
     def __init__(self, name, description, price, quantity):
+        """Инициализация товара."""
         self.name = name
         self.description = description
         self.__price = price
@@ -14,23 +15,23 @@ class Product:
 
     @classmethod
     def new_product(cls, new_product: dict) -> "Product":
+        """Создает новый товар из словаря данных."""
         return cls(new_product["name"], new_product["description"], new_product["price"], new_product["quantity"])
 
     @property
-    def price(self):
+    def price(self) -> float:
         """Геттер для цены"""
         return self.__price
 
     @price.setter
-    def price(self, new_price):
+    def price(self, new_price) -> None:
         """Сеттер для цены с проверкой"""
         if new_price <= 0:
             print("Цена не должна быть нулевая или отрицательная")
         else:
             if new_price < self.__price:  # если цена понижается
                 confirmation = input(f"Вы уверены, что хотите понизить цену с {self.__price} до {new_price}? (y/n): ")
-                if confirmation.lower() == 'y':
+                if confirmation.lower() == "y":
                     self.__price = new_price
             else:
                 self.__price = new_price
-
